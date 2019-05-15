5feccf9c91f81fcf701fa8abf1f89055fdd69053
吴正华 <351563763@qq.com>
2019-05-15 16:37:34 +0800

    - debug

--- app/src/main/java/com/ilifesmart/model/MOBase.java
+++ app/src/main/java/com/ilifesmart/model/MOBase.java
@@ -1,32 +1,42 @@
 package com.ilifesmart.model;
 
+import com.ilifesmart.listener.INotiListener;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class MOBase {
 
-//	private List<INotiListener> registers = Collections.synchronizedList(new ArrayList<>());
-//
-//	public void registerListener(INotiListener listener) {
-//		if (listener != null) {
-//			if (!registers.contains(listener)) {
-//				registers.add(listener);
-//			}
-//		}
-//	}
-//
-//	public void unRegisterListener(INotiListener listener) {
-//		if (listener != null) {
-//			if (registers.contains(listener)) {
-//				registers.remove(listener);
-//			}
-//		}
-//	}
-//
-//	public int getListenersSize() {
-//		return registers.size();
-//	}
+	private List<INotiListener> registers = Collections.synchronizedList(new ArrayList<>());
+
+	public void registerListener(INotiListener listener) {
+		if (listener != null) {
+			if (!registers.contains(listener)) {
+				registers.add(listener);
+			}
+		}
+	}
+
+	public void unRegisterListener(INotiListener listener) {
+		if (listener != null) {
+			if (registers.contains(listener)) {
+				registers.remove(listener);
+			}
+		}
+	}
+
+	public int getListenersSize() {
+		return registers.size();
+	}
 
 	public String cgy;
 	public String uuid;
 	public String title;
+
+	public MOBase() {
+
+	}
 
 	public MOBase(String cgy, String uuid) {
 		this.cgy = cgy;
