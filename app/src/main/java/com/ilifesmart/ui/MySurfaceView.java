fc8248e0730cecae090c06515970388247a83706
吴正华 <351563763@qq.com>
2019-05-30 14:06:53 +0800

    -
    蛛网实现，基于SurefaceView。
    SurefaceVew:双缓冲

--- app/src/main/java/com/ilifesmart/ui/MySurfaceView.java
+++ app/src/main/java/com/ilifesmart/ui/MySurfaceView.java
@@ -2,68 +2,62 @@ package com.ilifesmart.ui;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
 import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
-public class WarriorSurfaceView extends SurfaceView implements SurfaceHolder.Callback {
+public abstract class MySurfaceView extends SurfaceView implements SurfaceHolder.Callback {
 
-	private Context mContext;
+	protected Context mContext;
 	private SurfaceHolder mHolder;
-	private LoopThread thread;
+	private MySurfaceView.LoopThread thread;
 
-
-	public WarriorSurfaceView(Context context) {
+	protected MySurfaceView(Context context) {
 		this(context, null);
 	}
 
-	public WarriorSurfaceView(Context context, AttributeSet attrs) {
+	protected MySurfaceView(Context context, AttributeSet attrs) {
 		this(context, attrs, 0);
 	}
 
-	public WarriorSurfaceView(Context context, AttributeSet attrs, int defStyleAttr) {
+	protected MySurfaceView(Context context, AttributeSet attrs, int defStyleAttr) {
 		super(context, attrs, defStyleAttr);
 
-		this.mContext = context;
-		initialize(attrs);
-	}
-
-	private void initialize(AttributeSet attrs) {
+		mContext = context;
 		mHolder = getHolder();
-		setZOrderOnTop(true);
 		mHolder.addCallback(this);
 
-		thread = new LoopThread(getContext(), mHolder);
+		thread = new LoopThread(context, mHolder);
 	}
 
 	@Override
 	public void surfaceCreated(SurfaceHolder holder) {
 		thread.isRunning = true;
 		thread.start();
+		onSurfaceCreated(holder);
 	}
 
 	@Override
 	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-
+		onSurfaceChanged(holder, format, width, height);
 	}
 
 	@Override
 	public void surfaceDestroyed(SurfaceHolder holder) {
 		thread.isRunning = false;
 		try {
-			thread.join(); // 等待线程死去
-		} catch (Exception ex) {
+			thread.join();
+		} catch(Exception ex) {
 			ex.printStackTrace();
 		}
+		onSurfaceDestroyed(holder);
 	}
 
-	private class LoopThread extends Thread {
+	public class LoopThread extends Thread {
 		SurfaceHolder mSurfaceHolder;
 		Context mContext;
 		boolean isRunning;
-		float radius = 10f;
 		Paint paint;
 
 		public LoopThread(Context context, SurfaceHolder holder) {
@@ -71,9 +65,6 @@ public class WarriorSurfaceView extends SurfaceView implements SurfaceHolder.Cal
 			this.mSurfaceHolder = holder;
 			paint = new Paint(Paint.ANTI_ALIAS_FLAG);
 			isRunning = false;
-
-			paint.setColor(Color.YELLOW);
-			paint.setStyle(Paint.Style.STROKE);
 		}
 
 		@Override
@@ -86,24 +77,24 @@ public class WarriorSurfaceView extends SurfaceView implements SurfaceHolder.Cal
 				try {
 					synchronized (mSurfaceHolder) {
 						canvas = mSurfaceHolder.lockCanvas(null); // surfaceview会保留之前的图形，此处清空之前的图形
-						doDraw(canvas);
-						Thread.sleep(50);
+						doDraw(canvas, paint);
 					}
 				} catch (Exception ex) {
 					ex.printStackTrace();
 				} finally {
-					mSurfaceHolder.unlockCanvasAndPost(canvas); // 释放锁
+					mSurfaceHolder.unlockCanvasAndPost(canvas); // canvas生效
 				}
 			}
 		}
 
-		public void doDraw(Canvas canvas) {
-			canvas.drawColor(Color.GRAY);
-			canvas.translate(200, 200);
-			canvas.drawCircle(0,0,radius++, paint);
-
-			radius = (radius > 100) ? 10f : radius;
-		}
 	}
+
+	public abstract void doDraw(Canvas canvas, Paint paint);
+
+	public void onSurfaceCreated(SurfaceHolder holder) {}
+
+	public void onSurfaceChanged(SurfaceHolder holder, int format, int width, int height) {}
+
+	public void onSurfaceDestroyed(SurfaceHolder holder) {}
 
 }
