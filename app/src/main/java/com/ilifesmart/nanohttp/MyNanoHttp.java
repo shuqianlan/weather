edbe1179b3a6a2f454052ee6c3d75fd0b282a137
wuzhxyl <351563763@qq.com>
2024-01-16 18:08:41 +0800

    - json
    1. 修改为json返回，支持自定义输出

--- app/src/main/java/com/ilifesmart/nanohttp/MyNanoHttp.java
+++ app/src/main/java/com/ilifesmart/nanohttp/MyNanoHttp.java
@@ -9,6 +9,7 @@ import com.google.gson.JsonObject;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -17,8 +18,8 @@ import fi.iki.elonen.NanoHTTPD;
 public class MyNanoHttp extends NanoHTTPD {
     public static final String TAG = "NanoHttpD";
     private String rootPath;
-    String file_none_miss = "<html><body><h1>文件不存在</h1></body></html>";
-    String err_params = "<html><body><h1>无效的格式</h1></body></html>";
+    String file_none_miss = "{\"code\": 1, \"msg\": \"文件不存在\"}";
+    String err_params = "{\"code\": 1, \"msg\": \"无效的格式\"}";
 
     public MyNanoHttp(int port, String _rootPath) {
         super(port);
@@ -45,14 +46,14 @@ public class MyNanoHttp extends NanoHTTPD {
                     FileInputStream inputStream = new FileInputStream(file);
                     return newFixedLengthResponse(Response.Status.OK, getMimeTypeForFile(filePath), inputStream, file.length());
                 } else {
-                    return newFixedLengthResponse(Response.Status.BAD_REQUEST, "text/html", file_none_miss);
+                    return newFixedLengthResponse(Response.Status.BAD_REQUEST, "application/json", file_none_miss);
                 }
 
             } catch (Throwable ex) {
-                return newFixedLengthResponse(Response.Status.BAD_REQUEST, "text/html", file_none_miss);
+                return newFixedLengthResponse(Response.Status.BAD_REQUEST, "application/json", file_none_miss);
             }
         }
 
-        return newFixedLengthResponse(Response.Status.BAD_REQUEST, "text/html", err_params);
+        return newFixedLengthResponse(Response.Status.BAD_REQUEST, "application/json", err_params);
     }
 }
